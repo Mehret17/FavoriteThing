fce089a9ecb51b8779d9dcc4d57608cc995043bc
Mehret Kulala <merutaye@gmail.com>
2018-09-01 11:44:51 -0500

    first draft complete

---
+++ FavoriteThings/Foods/Sushi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings.Foods
+{
+    class Sushi : FoodBase
+    {
+        // property
+        //public string Type { get; set; } = "vegan california roll sushi";
+        //public bool isOrganic { get; set; } = true;
+
+        //method
+        //public void GetType()
+        //{
+        //    Console.WriteLine($"my favoriet food is {Type}");
+        //}
+
+        //public void Organic()
+        //{
+
+        //    if (isOrganic)
+        //    {
+        //        Console.WriteLine("It very healthy");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No thanks, that is not healty!");
+        //    }
+        //}
+        public Sushi() : base("sushi")
+        {
+        }
+    }
+}
--- FavoriteThings/Program.cs
+++ FavoriteThings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FavoriteThings.Foods;
 
 namespace FavoriteThings
 {
@@ -6,7 +7,38 @@ namespace FavoriteThings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var burger = new Burger();
+            var sushi = new Sushi();
+            var pizza = new Pizza();
+
+            var foods = new FoodBase[] { burger, sushi, pizza };
+
+            foreach (var food in foods)
+            {
+                food.GetType();
+                food.Organic();
+            }
+
+            //burger.GetType();
+            //burger.Organic();
+            //Console.WriteLine();
+
+           
+            //sushi.GetType();
+            //sushi.Organic();
+            //Console.WriteLine();
+
+         
+            //pizza.GetType();
+            //pizza.Organic();
+            //Console.WriteLine();
+
+
+            Console.ReadLine();
+
+   
+
+
         }
     }
 }
